266e2406a02a69e3a0806e7a2eaefb97fee35c5c
Alan Fung <afung63@my.bcit.ca>
2022-03-09 09:54:15 -0800

    Added a calendar functionality

---
+++ scripts/calendar.js
@@ -0,0 +1,59 @@
+let date = new Date();
+
+function loadCalendar() {
+    document.querySelector("#monthyear").innerHTML = date.toLocaleString('default', {
+        month: 'long',
+        year: 'numeric'
+    });
+    document.querySelector("#current").innerHTML = new Date().toLocaleString('default', {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+    });
+
+    let days = "";
+
+    let monthdays = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    let day = 1;
+    let prevmonthdays = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+    let prevmonthday = new Date(date.getFullYear(), date.getMonth(), 0).getDate() - prevmonthdays + 1;
+    let nextmonthdays = 7 - new Date(date.getFullYear(), date.getMonth() + 1, 1).getDay();
+    let nextmonthday = 1;
+
+    document.querySelector("#days").innerHTML = "";
+
+    for (let i = 0; i < (monthdays + prevmonthdays + nextmonthdays); i++) {
+        if (i < prevmonthdays) {
+            document.querySelector("#days").insertAdjacentHTML('beforeend', `<div class="othermonth"><span>${prevmonthday}</span></div>`);
+            prevmonthday++;
+        } else if (day === new Date().getDate() && date.getMonth() === new Date().getMonth() && date.getFullYear() === new Date().getFullYear()) {
+            document.querySelector("#days").insertAdjacentHTML('beforeend', `<div id="currentday" class="bg-info"><span>${day}</span></div>`);
+            day++;
+        } else if (day <= monthdays) {
+            document.querySelector("#days").insertAdjacentHTML('beforeend', `<div><span>${day}</span></div>`);
+            day++;
+        } else if (nextmonthday <= nextmonthdays && nextmonthdays != 7) {
+            document.querySelector("#days").insertAdjacentHTML('beforeend', `<div class="othermonth"><span>${nextmonthday}</span></div>`);
+            nextmonthday++;
+        }
+    }
+}
+
+function prevmonth() {
+    date.setMonth(date.getMonth() - 1);
+    loadCalendar();
+}
+
+function nextmonth() {
+    date.setMonth(date.getMonth() + 1);
+    loadCalendar();
+}
+
+function currentmonth() {
+    date.setFullYear(new Date().getFullYear())
+    date.setMonth(new Date().getMonth())
+    loadCalendar();
+}
+
+loadCalendar();
