a7e19da8e403df30cc6fa35bdf65c84ed31b8758
Alan Fung <afung63@my.bcit.ca>
2022-03-11 20:05:13 -0800

    Added calendar weekview.

---
+++ scripts/weekview.js
@@ -0,0 +1,67 @@
+let date = new Date();
+
+function loadCalendar() {
+    document.querySelector("#monthyear").innerHTML = date.toLocaleString('default', {
+        month: 'long',
+        year: 'numeric'
+    });
+    document.querySelector("#current").innerHTML = new Date().toLocaleString('default', {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+    });
+
+    let days = "";
+
+    let prevmonthdays = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+    let prevmonthday = new Date(date.getFullYear(), date.getMonth(), 0).getDate() - prevmonthdays + 1;
+    let monthdays = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    let today = date.getDate();
+    let todayday = date.getDay();
+    console.log(today);
+    console.log(todayday);
+    let day = today - todayday;
+    console.log(day);
+    let nextmonthdays = 7 - new Date(date.getFullYear(), date.getMonth() + 1, 1).getDay();
+    let nextmonthday = 1;
+
+    document.querySelector("#days").innerHTML = "";
+
+    for (let i = 0; i < 7; i++) {
+        if (day <= 0 && i < prevmonthdays) {
+            document.querySelector("#days").insertAdjacentHTML('beforeend', `<div><span>${prevmonthday}</span></div>`);
+            prevmonthday++;
+        } else if (day === new Date().getDate() && date.getMonth() === new Date().getMonth() && date.getFullYear() === new Date().getFullYear()) {
+            document.querySelector("#days").insertAdjacentHTML('beforeend', `<div id="currentday" class="bg-info"><span>${day}</span></div>`);
+            day++;
+        } else if (day <= monthdays) {
+            if (day <= 0) {
+                day = 1;
+            }
+            document.querySelector("#days").insertAdjacentHTML('beforeend', `<div><span>${day}</span></div>`);
+            day++;
+        } else if (nextmonthday <= nextmonthdays && nextmonthdays != 7) {
+             document.querySelector("#days").insertAdjacentHTML('beforeend', `<div><span>${nextmonthday}</span></div>`);
+             nextmonthday++;
+        }
+    }
+}
+
+function prevmonth() {
+    date.setDate(date.getDate() - 7);
+    loadCalendar();
+}
+
+function nextmonth() {
+    date.setDate(date.getDate() + 7);
+    loadCalendar();
+}
+
+function currentmonth() {
+    date.setFullYear(new Date().getFullYear())
+    date.setMonth(new Date().getMonth())
+    loadCalendar();
+}
+
+loadCalendar();
