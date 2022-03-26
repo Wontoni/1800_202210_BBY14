76a121e2c9609d0d5f0e6a6b6397dfb27b2d0c4b
Usman Arshad <uarshad2012@gmail.com>
2022-03-25 19:19:33 -0700

    day view

--- scripts/dayview.js
+++ scripts/dayview.js
@@ -1,4 +1,25 @@
+let date = new Date();
 
+function loadCalendar() {
+    document.querySelector("#monthyear").innerHTML = date.toLocaleString('default', {
+        month: 'long',
+        year: 'numeric',
+        day: 'numeric'
+    });
+    document.querySelector("#current").innerHTML = new Date().toLocaleString('default', {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+    });
+    document.querySelector("#weekday").innerHTML = date.toLocaleString('default', {
+        weekday: 'long'
+    });
+
+    let today = date.getDate();
+    let todayday = date.getDay();
+    let day = today - todayday;
+}
 
 function prevday() {
     date.setDate(date.getDate() - 1);
@@ -13,5 +34,8 @@ function nextday() {
 function currentday() {
     date.setFullYear(new Date().getFullYear())
     date.setMonth(new Date().getMonth())
+    date.setDate(new Date().getDate())
     loadCalendar();
 }
+
+loadCalendar();
--- scripts/event.js
+++ scripts/event.js
@@ -1,33 +1,6 @@
 function writeEvent() {
-    let Event = document.getElementById("eventTitle").value;
-    let Time = document.getElementById("date").value;
-    let Duration = document.getElementById("time").value;
+    let Title = document.getElementById("title").value;
+    let Date = document.getElementById("date").value;
+    let Time = document.getElementById("time").value;
     let Location = document.getElementById("location").value;
-
-
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            var currentUser = db.collection("event").doc(user.uid);
-            var userID = user.uid;
-            //get the document for current user.
-            currentUser.get()
-                .then(userDoc => {
-                    // Start a new collection and add all data in it.
-                    db.collection("events").add({
-                        code: eventID,
-                        userID: userID,  //for logged in user
-                        eventTitle: Event,
-                        time: Time,
-                        date: Date,
-                        location: Location,
-                    })
-                    .then(function(){
-                    window.location.href = "calendar.html"
-                    });
-                })
-        } else {
-            // No user is signed in.
-            console.log("no user signed in");
-        }
-    });
 }
