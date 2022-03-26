4facf00bdb7beadf37433afc9224e7ee869d1fa5
Wontonious <48232238+Wontonious@users.noreply.github.com>
2022-03-26 16:27:44 -0700

    Updated todo List

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,35 +1,35 @@
 // Open/Close To-Do List
-var myOffcanvas = document.getElementById('offcanvasWithBackdrop')
-var bsOffcanvas = new bootstrap.Offcanvas(offcanvasWithBackdrop)
-
-function toggleToDo(){
+var myOffcanvas = document.getElementById('todoOffCanvas')
+var bsOffcanvas = new bootstrap.Offcanvas(todoOffCanvas)
+function toggleToDo() {
   bsOffcanvas.toggle();
 }
 
 // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
+var myNodelist = document.getElementsByClassName("listItem");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
-  span.className = "close";
+  span.className = "closeTask";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
+  console.log(myNodelist[i]);
 }
 
 // Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
+var close = document.getElementsByClassName("closeTask");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
   }
 }
 
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
+var list = document.getElementById("todoList");
+list.addEventListener('click', function (ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
@@ -38,26 +38,66 @@ list.addEventListener('click', function(ev) {
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
+  li.className = "listItem";
+  var inputValue = document.getElementById("newToDo").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
+    document.getElementById("todoList").appendChild(li);
+    document.getElementById("newToDo").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "closeTask";
+    span.appendChild(txt);
+    li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+    document.getElementById("newToDo").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "closeTask";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
     }
   }
+
+  let Task = document.getElementById("newToDo").value;
+
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      var currentUser = db.collection("users").doc(user.uid);
+      var userID = user.uid;
+      //get the document for current user.
+      currentUser.get()
+        .then(userDoc => {
+          // Start a new collection and add all data in it.
+          db.collection("users").doc(user.uid).collection("To-Do-List").add({
+            userID: userID,  //for logged in user
+            name: Task,
+            completion: Complete,
+          })
+            .then(function () {
+              console.log("Successfully added task");
+            });
+        })
+    } else {
+      // No user is signed in.
+      console.log("no user signed in");
+    }
+  });
+
+
+
 }
+
+
+
