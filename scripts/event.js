5c9caf22d4e0ec227222d554660c7a0e6d0e07d9
Usman Arshad <uarshad2012@gmail.com>
2022-03-30 09:56:16 -0700

    event write funtion to database

--- scripts/event.js
+++ scripts/event.js
@@ -1,6 +1,32 @@
 function writeEvent() {
-    let Title = document.getElementById("title").value;
+    console.log("in")
+    let EventName = document.getElementById("eventName").value;
     let Date = document.getElementById("date").value;
     let Time = document.getElementById("time").value;
     let Location = document.getElementById("location").value;
+    console.log(EventName, Date, Time, Location);
+
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            var currentUser = db.collection("users").doc(user.uid)
+            var userID = user.uid;
+
+            currentUser.get()
+            .then(userDoc => {
+                db.collection("Events").add({
+                userID: userID,
+                eventName: EventName,
+                time: Time,
+                date: Date,
+                location: Location
+            })
+
+        })
+        } else {
+            console.log ("No user is signed in");
+        }
+    });
+
+
 }
