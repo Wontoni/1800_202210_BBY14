546ac22458865e3816d8f14a6e7e18032341d89e
Alan Fung <afung63@my.bcit.ca>
2022-03-31 00:55:05 -0700

    create and join group function added

--- scripts/group.js
+++ scripts/group.js
@@ -9,27 +9,65 @@ function createGroup() {
             var userID = user.uid;
 
             currentUser.get()
-            .then(userDoc => {
-                db.collection("Groups").add({
-                OwnerID: userID,
-                groupName: groupName,
-                groupCode: generateCode()
-
-            })
-            .then(()=>{
-                window.location.href = "";
-            })
-
-        })
+                .then(userDoc => {
+                    db.collection("Groups").add({
+                            OwnerID: userID,
+                            users: firebase.firestore.FieldValue.arrayUnion(userID),
+                            groupName: groupName,
+                            groupCode: generateCode()
+                        }, {
+                            merge: true
+                        })
+                        .then(() => {
+                            window.location.href = "";
+                        })
+                })
         } else {
-            console.log ("No user is signed in");
+            console.log("No user is signed in");
         }
     });
-
 }
 
-function generateCode(){
-    let characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function joinGroup() {
+    console.log("in")
+    let groupCode = document.getElementById("groupCode").value;
+    console.log(groupCode);
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            var currentUser = db.collection("users").doc(user.uid)
+            var userID = user.uid;
+            var groupID;
+            console.log(userID);
+            db.collection("Groups").where("groupCode", "==", groupCode).get().then(snap => {
+                size = snap.size;
+                queryData = snap.docs;
+                if (size == 1) {
+                    groupID = queryData[0].id;
+                } else {
+                    console.log("Query has more than one data")
+                }
+            })
+            console.log(groupID);
+            currentUser.get()
+                .then(userDoc => {
+                    db.collection("Groups").doc(groupID).set({
+                            users: firebase.firestore.FieldValue.arrayUnion(userID)
+                        }, {
+                            merge: true
+                        })
+                        .then(() => {
+                            window.location.href = "";
+                        })
+                })
+        } else {
+            console.log("No user is signed in");
+        }
+    });
+}
+
+function generateCode() {
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let code = "";
     for (let i = 0; i < 6; i++) {
         code += characters.charAt(Math.floor(Math.random() * characters.length));
