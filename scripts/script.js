d8c7ebf3e1e028e6de81407e6226ed7dfe0977f6
Usman Arshad <uarshad2012@gmail.com>
2022-04-02 14:23:52 -0700

    display user name on nav bar

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,18 @@ function sayHello() {
     });
 }
 //sayHello();
+
+function insertName() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid)
+            currentUser.get()
+                .then(userDoc => {
+                    var user_name = userDoc.data().name;
+                    $("#name-goes-here").text(user_name);
+                })
+        } else {
+            console.log("No user is signed in");
+        }
+    });
+}
