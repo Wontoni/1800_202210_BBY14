08654494e19ef1f44570e1de0c3e5eddeea2bed8
Wontonious <48232238+Wontonious@users.noreply.github.com>
2022-04-03 20:43:37 -0700

    Added some styling

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-function openTab(evt, cityName) {
+function openTab(evt, tabName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -11,10 +11,10 @@ function openTab(evt, cityName) {
   // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace("active", "");
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += "active";
 }
